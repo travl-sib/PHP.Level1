69fd151885f4d60e72f92b98f0aaf8489b819e5a
travl-sib <truhin.andr@gmail.com>
2019-09-07 17:42:35 +0700

    Задание к 3 занятию

--- index.php
+++ index.php
@@ -1,138 +1,135 @@
 <?php
 // Задание 1
-    $a=mt_rand(-100,100);
-    $b=mt_rand(-100,100);
-    echo "a=".$a." ; "."b=".$b."<br>";
+ $i=0;
 
-    if (($a>=0) && ($b>=0)){ 
-        echo $a-$b;
-    }
-   elseif (($a<0) && ($b<0)) {
-      echo $a*$b;
-    } 
-    else {
-        echo $a+$b;
-    }
+ while ( $i<= 100) {
+ 	if ($i%3 == 0){
+ 		echo $i." ";
+ 	}
+ 	$i++;
+ }
 
-echo '<br>';
     //Задание 2
-    $a=mt_rand(0,15);
-    switch ($a) {
-        case 0:
-            echo 0;
-        case 1:
-            echo 1;
-        case 2:
-            echo 2;
-        case 3:
-            echo 3;
-        case 4:
-            echo 4;
-        case 5:
-            echo 5;
-        case 6:
-            echo 6;
-        case 7:
-            echo 7;
-         case 8:
-            echo 8;
-        case 9:
-            echo 9;
-        case 10:
-            echo 10;
-        case 11:
-            echo 11;
-        case 12:
-            echo 12;
-        case 13:
-            echo 13;
-        case 14:
-            echo 14;
-        case 15:
-            echo 15;
-        break;
-    }
-
+ echo '<br>';
+ $i= 0;
+do {
+  if ($i == 0) 
+  	echo $i." - ноль."."<br>";
+  elseif ($i%2 == 0) 
+  	echo $i." - четное число."."<br>";
+  else 
+  	echo $i." - нечетное число."."<br>";
+  $i++;
+}
+while ( $i<= 10) ;
 
     //Задание3
-function add($a,$b) {
-    return $a+$b;
+$regions= [
+  'Московская область'=> ['Москва', 'Зеленоград', 'Клин'],
+  'Ленинградская область'=> ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
+  'Рязанская область'=> ['Касимов', 'Кораблино', 'Михайлов', 'Новомичуринск', 'Рыбное']
+ // 'Красноярский край'=> ['', '', '', '', ''],
+  //'Новосибирская область'=> ['', '', '', '', ''],
+];
+foreach ($regions as $region => $citys) {
+	echo $region.':<br>';
+	foreach ($citys as $key => $city) {
+		$sign=', ';
+		if ($key== (count($citys)-1))
+			$sign='.<br>';
+		echo $city.$sign;
+	}
 }
-
-function subtract($a,$b) {
-    return $a-$b;
-}
-
-function multiply($a,$b) {
-    return $a*$b;
-}
-
-function divide($a,$b) {
-    return $a/$b;
-}
-
 
     //Задание4
-function mathOperation($arg1, $arg2, $operation="Сложение"){
-    switch ($operation) {
-        case 'Сложение':
-           echo add($arg1, $arg2);
-            break;
-        case 'Вычитание':
-           echo subtract($arg1, $arg2);
-            break;
-        case 'Умножение':
-           echo multiply($arg1, $arg2);
-            break;
-        case 'Деление':
-           echo divide($arg1, $arg2);
-            break;
-}
-}
-echo '<br>';
-mathOperation (5, 6, "Умножение");
+
+$translit= ['а'=>'a', 'б'=>'b', 'в'=>'v', 'г'=>'g', 'д'=>'d', 'е'=>'e', 'ё'=>'yo', 'ж'=>'zh', 'з'=>'z', 'и'=>'i', 'й'=>'y', 'к'=>'k', 'л'=>'l', 'м'=>'m', 'н'=>'n', 'о'=>'o', 'п'=>'p', 'р'=>'r', 'с'=>'s', 'т'=>'t', 'у'=>'u', 'ф'=>'f', 'х'=>'kh', 'ц'=>'ts', 'ч'=>'ch', 'ш'=>'sh', 'щ'=>'shch', 'ъ'=>'″', 'ы'=>'y', 'ь'=>'′', 'э'=>'e', 'ю'=>'yu', 'я'=>'ya'];
+
+function getTranslit($str){
+	global $translit;
+	$result=strtr($str, $translit);
+ 	return $result;
+ }
+echo "<br>";
+echo getTranslit('китай');
+
+    //Задание5
+function getUnderline($str){
+	$result=strtr($str, ' ', "_");
+ 	return $result;
+ }
+echo "<br>";
+echo getUnderline('китай будет там');
+echo "<br>";
 
     //Задание6
-function power($val, $pow){
-    if ($pow==1){
-        return $val;
-    }
-    return $val*power($val, $pow-1);
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>lesson3</title>
+  </head>
+  <body>
+
+<ul class="nav nav-tabs">
+
+ <?php foreach ($regions as $region => $citys): ?>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?= $region ?></a>
+    <div class="dropdown-menu">
+    	<?php foreach ($citys as $num => $city): ?>
+      <a class="dropdown-item" href="#"><?= $city ?></a>
+         <?php endforeach;?>
+          </div>
+  </li>
+
+ <?php endforeach;?>
+</ul>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+
+<?php
+
+//Задание7
+for ($i=0; $i < 10; print($i), $i++) { 
+};
+
+//Задание8
+echo "<br>";
+
+foreach ($regions as $region => $citys) {
+	echo $region.':<br>';
+	foreach ($citys as $key => $city) {
+		$city1=preg_split('//u', $city, null,PREG_SPLIT_NO_EMPTY);
+		if ($city1[0] != 'К') { 
+			continue;}
+		$sign=', ';
+		if ($key== (count($citys)-1))
+			$sign='.<br>';
+		echo $city.$sign;
+	}
 }
 
-echo '<br>';
-echo power (3,4);
+//Задание9
 
-
-
-    //Задание7
-
-function getHours(){
-    $hour=date('H',time());
-    if (($hour==1) || ($hour==21)){
-        return $hour." час";
-    }
-    elseif ((($hour>1)&&($hour<5))||($hour>21)){
-        return $hour." часа";
-    }
-    return $hour." часов";
+function changeStr($str) {
+return getUnderline (getTranslit($str));	
 }
-
-function getMinutes(){
-    $min=date('i',time());
-    if (($min==1) || ($min[1]==1)){
-        return $min." минута";
-    }
-    elseif ((($min>1)&&($min<5))||(($min[1]>1)&&($min[1]<5))){
-        return $min." минуты";
-    }
-    return $min." минут";
-}
-
-function currentTime(){
-    echo getHours()." ".getMinutes();
-}
-
-echo '<br>';
-currentTime()
+echo "<br>";
+echo changeStr('китай будет там');
 ?>
